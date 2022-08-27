2d777cd589ca4821c3234a03bbd96f97ec1f4bbe
Shaban27 <l-shaban@yandex.ru>
2022-08-28 02:25:40 +0900

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,35 @@
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
         printLeapYear(2024);
-        printOperatingSystem(2014, 0);
+        printOperatingSystem(2015, 0);
         printDeliveryDistance(95);
     }
 
     public static void printLeapYear(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println (year + "год високосный");
+            System.out.println(year + "год високосный");
         } else {
-            System.out.println (year + "год не високосный");
-        }}
-
-
-    public static void printOperatingSystem(int product, int clientOS) {
-        String version = product < 2015 ? " lite" : "";
-        String os = clientOS == 0 ? "iOS" : " Android";
-        System.out.printf("УСТАНОВИТЬ %S ВЕРСИЮ ПРИЛОЖЕНИЯ ДЛЯ %S ПО ССЫЛКЕ", version, os);
-        if (product < 2015) {
-            System.out.println("Установить lite");
-        } else {
-            System.out.println("Установить обычную");
-            if (clientOS == 0) {
-                System.out.println(" iOS ");
-            } else {
-                System.out.println(" Android ");
-            }
+            System.out.println(year + "год не високосный");
         }
+    }
+
+
+    public static void printOperatingSystem(int productY, int clientOS) {
+        if (!(clientOS == 0 || clientOS == 1)) {
+            throw new RuntimeException("устройство");
+        }
+        var currentYear = LocalDate.now().getYear();
+        String version = productY < currentYear ? " lite" : "";
+        String os = (clientOS == 0 ? "iOS" : "android");
+        System.out.printf ("установить %s версию для %s", version, os  );
 
     }
-    public static void printDeliveryDistance(int distance) {
+
+
+    public static void printDeliveryDistance (int distance) {
         int deliveryDays = 1;
         int lastDistance = 40;
         int firstDistance = 20;
